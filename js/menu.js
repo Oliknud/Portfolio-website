3c8880ca1ba634bc60b1d1602abd7ad6d3c01f98
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-06-06 22:22:55 +0200

    .

--- js/menu.js
+++ js/menu.js
@@ -8,14 +8,17 @@ burger.addEventListener("click", burgermenu);
 x.addEventListener("click", burgermenu);
 
 function burgermenu() {
+    
     // When toggled; slides in menu from left
     menuLinks.classList.toggle("mobile-links-active");
-
+    
     // If statement to hide overflow when menu is active.
     if (body.style.overflow === "hidden") {
         body.style.overflow = "visible";
+        
     } else {
         body.style.overflow = "hidden";
+        window.scrollTo(0, 0);
     }
 }
 
