0835cb2fd9110704daf56f148c4c29cbdf85f5bb
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-06-06 22:15:35 +0200

    .

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+const menuLinks = document.querySelector(".mobile-nav");
+const burger = document.querySelector("#burger");
+const x = document.querySelector(".cross");
+const body = document.querySelector("body");
+
+// Adding click events to menu icon and X icon
+burger.addEventListener("click", burgermenu);
+x.addEventListener("click", burgermenu);
+
+function burgermenu() {
+    // When toggled; slides in menu from left
+    menuLinks.classList.toggle("mobile-links-active");
+
+    // If statement to hide overflow when menu is active.
+    if (body.style.overflow === "hidden") {
+        body.style.overflow = "visible";
+    } else {
+        body.style.overflow = "hidden";
+    }
+}
+
